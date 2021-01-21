49729da7d38a9939e973e65bda1554b22d430601
wesley guerra <guerra_ssa@hotmail.com>
2021-01-20 22:26:07 -0300

    create connection success database

--- server.js
+++ server.js
@@ -1,16 +1,27 @@
 const express = require('express');
 const app = express();
 const bodyParser = require("body-parser");
+const connection = require('./database/database');
 
 //View engine
 app.set('view engine', 'ejs');
-// stattic
+
+// static
 app.use(express.static('public'));
 
 //Body parser forms
-app.use(bodyParser.urlencoded({ extends: false }));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+//database connection
+connection.authenticate()
+    .then(() => {
+        console.log('Conexão database feita com sucesso!')
+    }).catch(erro => {
+        console.log(erro)
+    });
+
+//rotas
 app.get('/', (req, res) => {
     res.render("index");
 });
