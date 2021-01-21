c8d82131b79a007b31358c62c8b5cdb2b040fd05
wesley guerra <guerra_ssa@hotmail.com>
2021-01-21 08:35:50 -0300

    configurando roatas articles

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const app = express();
 const bodyParser = require("body-parser");
 const connection = require('./database/database');
 const categoriesController = require('./categories/CategoriesController');
+const articlesController = require('./articles/ArticlesController');
 
 //View engine
 app.set('view engine', 'ejs');
@@ -23,8 +24,8 @@ connection.authenticate()
     });
 
 //rotas
-app.use('/', categoriesController); // utilizar rotas que estão dentro desse arquivo
-
+app.use('/', categoriesController); // rotas de categorais
+app.use('/', articlesController) // rotas de articles
 
 app.listen(8080, () => {
     console.log("O servidor está rodando!");
