cf83ef3a32a1b43ee559ea53003eb7d50301540c
wesley guerra <guerra_ssa@hotmail.com>
2021-01-25 10:18:51 -0300

    criando rotas de autenticação sanvando no bando

--- user/UsersController.js
+++ user/UsersController.js
@@ -4,12 +4,17 @@ const User = require("./User");
 
 
 router.get("/admin/users", (req, res) => {
-
+    //listagem de usuários
 });
 
 
 router.get("/admin/users/create", (req, res) => {
-
+    res.render("admin/users/create")
 });
+
+router.post("/users/create", (req, res) => {
+    const { email, password } = req.body;
+
+})
 
 module.exports = router;
